ad7aefc6b8c243362266fa1bbf9a5373a46e620d
Milind <40736631+milindex@users.noreply.github.com>
2025-03-31 10:40:33 +0530

    Revert "Testing"
    This reverts commit d3c5be482b1dd8d8133e2690d275ad7bab566c75.

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" kamlesh="false">
+    <html lang="en">
       <body>
         <Header />
         {children}
